15240ecc1563e30572aca674ad6aca6b12fee407
Mutisoo <emutisom@gmail.com>
2026-09-04 18:55:26 +0300

    trust proxy headers for reverse-proxy deployment

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,8 @@ import express from 'express';
 import { join } from 'node:path';
 
 const browserDistFolder = join(import.meta.dirname, '../browser');
-
 const app = express();
-const angularApp = new AngularNodeAppEngine();
+const angularApp = new AngularNodeAppEngine({ trustProxyHeaders: true });
 
 /**
  * Example Express Rest API endpoints can be defined here.
